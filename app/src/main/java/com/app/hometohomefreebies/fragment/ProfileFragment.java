7f9910932d0d4e512a7961b2e9871e4159f4b037
ahmedabdeen <aabdeen@fcih1.com>
2022-10-21 14:08:44 +0200

    - Added logout option

--- app/src/main/java/com/app/hometohomefreebies/fragment/ProfileFragment.java
+++ app/src/main/java/com/app/hometohomefreebies/fragment/ProfileFragment.java
@@ -1,6 +1,9 @@
 package com.app.hometohomefreebies.fragment;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,18 +12,21 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.app.hometohomefreebies.R;
 import com.app.hometohomefreebies.activity.EditProfileActivity;
+import com.app.hometohomefreebies.activity.SplashActivity;
 import com.app.hometohomefreebies.activity.ViewProductActivity;
 import com.app.hometohomefreebies.adapter.ProfileProductsAdapter;
 import com.app.hometohomefreebies.app.Config;
 import com.app.hometohomefreebies.databinding.FragmentProfileBinding;
 import com.app.hometohomefreebies.model.Product;
 import com.bumptech.glide.Glide;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 public class ProfileFragment extends Fragment implements ProfileProductsAdapter.Interaction {
 
@@ -71,6 +77,31 @@ public class ProfileFragment extends Fragment implements ProfileProductsAdapter.
 
     private void initListeners(){
         binding.btnEditProfile.setOnClickListener(view -> startActivity(new Intent(requireContext(), EditProfileActivity.class)));
+        binding.btnLogout.setOnClickListener(view -> {
+            MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(requireContext(), R.style.AlertDialogTheme);
+
+            builder.setMessage("Are you sure you want to logout?");
+            builder.setCancelable(true);
+
+            builder.setPositiveButton("Yes", (dialogInterface, i) -> {
+                Intent intent = new Intent(requireContext(), SplashActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+
+                SharedPreferences pref = requireContext().getSharedPreferences(getString(R.string.pref_user_data), Context.MODE_PRIVATE);
+                pref.edit().clear().apply();
+            });
+
+            builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.cancel();
+                }
+            });
+
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+        });
     }
 
     private void initProductsRecyclerView(){
